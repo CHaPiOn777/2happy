8b6873569c79df7ff7f085a5e332c86f5d318670
Stoble <stoble@mail.ru>
2025-06-26 11:17:48 +0300

    feat(adaptive/orders): adaptive for orders card

--- src/features/Orders/components/OrderCard.tsx
+++ src/features/Orders/components/OrderCard.tsx
@@ -24,6 +24,7 @@ import { HELP_TABS } from "@/features/User/utils/isValidHelpTab";
 import StyledTooltip from "@/shared/components/UI/StyledTooltip";
 import { useState } from "react";
 import SuccessRefundDialog from "./SuccessRefundDialog";
+import Image from "next/image";
 
 const OrderCard = ({ order }: { order: OrderResponse }) => {
   const router = useRouter();
@@ -33,13 +34,15 @@ const OrderCard = ({ order }: { order: OrderResponse }) => {
 
   const { setCheckoutItems } = useCheckoutStore();
 
+  const mobileImages = order.line_items.slice(0, 2).map((item) => item.image);
+
   const getReturnDisabled = () => {
     switch (true) {
       case order.status === "refunded":
         return "Вы уже оформили возврат.";
       case !!order.date_paid &&
         differenceInDays(new Date(), new Date(order.date_paid)) > 20:
-        return "Срок оформления возврата истек.";
+        return "Срок возврата истек.";
       default:
         break;
     }
@@ -61,36 +64,47 @@ const OrderCard = ({ order }: { order: OrderResponse }) => {
   return (
     <article>
       <AccordionItem
-        className="border border-gray px-6"
+        className="border border-gray px-4 sm:px-6"
         value={`order_${order.id}`}
       >
         <AccordionTrigger className="py-4 text-body2">
-          <div className="w-full grid grid-cols-3">
-            <div className="flex flex-col gap-2">
+          <div className="w-full grid grid-cols-1 gap-y-4 sm:grid-cols-3">
+            <div className="flex sm:flex-col gap-2">
               <span>Дата заказа</span>
               <span className="text-gray-middle">
                 {`${format(new Date(order.date_created), "dd.MM.yyyy")}`}
               </span>
             </div>
-            <div className="flex flex-col gap-2">
+            <div className="hidden sm:flex flex-col gap-2">
               <span>Номер заказа</span>
               <span className="text-gray-middle">{order.number}</span>
             </div>
-            <div className="flex flex-col gap-2">
+            <div className="flex sm:hidden gap-2">
+              {mobileImages.map((image) => (
+                <Image
+                  key={image.id}
+                  src={image.src}
+                  alt="order-image"
+                  width={40}
+                  height={56}
+                />
+              ))}
+            </div>
+            <div className="flex sm:flex-col gap-2">
               <span>Сумма заказа</span>
               <span className="text-gray-middle">{order.total} ₸</span>
             </div>
           </div>
         </AccordionTrigger>
-        <AccordionContent className="space-y-14 pb-8">
+        <AccordionContent className="space-y-6 sm:space-y-8 md:space-y-14 pb-8">
           <div>
             {order.line_items.map((item) => (
               <OrderProductCard key={item.id} product={item} />
             ))}
           </div>
-          <div className="flex justify-between gap-4">
-            <div className="flex flex-col gap-14 max-w-[488px] w-full">
-              <div className="w-full flex flex-col gap-6">
+          <div className="grid grid-cols-1 grid-flow-row md:grid-cols-3 lg:grid-cols-[minmax(360px,512px),minmax(220px,312px),minmax(220px,312px)] gap-6 md:gap-4 md:gap-y-12">
+            <div className="flex flex-col gap-14 md:max-w-[488px] w-full">
+              <div className="w-full hidden lg:flex flex-col gap-6">
                 <Button
                   className="w-full"
                   onClick={handleRepeatOrder}
@@ -125,9 +139,11 @@ const OrderCard = ({ order }: { order: OrderResponse }) => {
                   setOpen={setSuccessRefund}
                 />
               </div>
-              <div className="flex gap-2 p-4 bg-order-card-help text-description">
-                <InfoBoldIcon className="shrink-0" />
-                <div className="flex flex-col gap-6">
+              <div className="flex gap-2 h-full lg:h-auto py-4 md:py-10 px-4 lg:p-4 bg-order-card-help text-description">
+                <div className="md:pt-6 lg:pt-0">
+                  <InfoBoldIcon className="shrink-0 " />
+                </div>
+                <div className="flex justify-center flex-col gap-6">
                   <div className="flex gap-2">
                     <p className="text-gray-dark">
                       Оформить возврат можно в течение 14 дней с момента
@@ -140,7 +156,7 @@ const OrderCard = ({ order }: { order: OrderResponse }) => {
                       </Link>
                     </p>
                   </div>
-                  <div className="flex justify-between">
+                  <div className="flex gap-4 lg:gap-0 flex-col lg:flex-row justify-between">
                     <span className="text-gray-dark">
                       Возникли вопросы по заказу? Свяжитесь с нами
                     </span>
@@ -150,50 +166,91 @@ const OrderCard = ({ order }: { order: OrderResponse }) => {
                       className="inline-flex items-center gap-2 link-hover"
                     >
                       <WhatsAppBoldIcon className="text-main" />
-                      <span className="text-button-xs">+7 (702) 165–73–78</span>
+                      <span className="text-button-xs text-nowrap">
+                        +7 (702) 165–73–78
+                      </span>
                     </a>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="flex gap-6">
-              <div className="w-[272px] border border-gray">
-                <h5 className="text-h5 pt-6 px-6 pb-4">Способ оплаты</h5>
-                <Separator className="w-full" />
-                <div className="flex flex-col p-6">
-                  <span className="uppercase">
-                    {order.payment_method_title}
-                  </span>
-                  <div className="flex justify-between border-b border-gray py-4 text-gray-dark">
-                    <span>Товары:</span>
-                    <span>{order.total} ₸</span>
-                  </div>
-                  <div className="flex justify-between border-b border-gray py-4 text-gray-dark">
-                    <span>Скидка:</span>
-                    <span>{order.discount_total} ₸</span>
-                  </div>
-                  <div className="flex justify-between text-h5 pt-6">
-                    <span>Итого:</span>
-                    <span>{order.total} ₸</span>
-                  </div>
+            <div className="border border-gray">
+              <h5 className="text-h5 pt-4 md:pt-6 px-4 sm:px-6 pb-4">
+                Способ оплаты
+              </h5>
+              <Separator className="w-full" />
+              <div className="flex flex-col p-4 sm:p-6">
+                <span className="uppercase text-button-small sm:text-body2 pb-2 md:pb-0">
+                  {order.payment_method_title}
+                </span>
+                <div className="flex justify-between text-button-xs sm:text-body2 border-b border-gray-light py-2 lg:py-4 text-gray-dark">
+                  <span>Товары:</span>
+                  <span>{order.total} ₸</span>
+                </div>
+                <div className="flex justify-between text-button-xs sm:text-body2 border-b border-gray-light py-2 lg:py-4 text-gray-dark">
+                  <span>Скидка:</span>
+                  <span>{order.discount_total} ₸</span>
+                </div>
+                <div className="flex justify-between text-h5 pt-4 md:pt-6">
+                  <span>Итого:</span>
+                  <span>{order.total} ₸</span>
                 </div>
               </div>
-
-              <div className="w-[272px] border border-gray">
-                <h5 className="text-h5 pt-6 px-6 pb-4">Адрес доставки</h5>
-                <Separator className="w-full" />
-                <div className="flex flex-col gap-4 p-6">
-                  <span className="text-body1">
-                    {order.billing.first_name} {order.billing.last_name}
-                  </span>
-                  <div className="flex flex-col gap-2 text-gray-dark text-body2">
-                    <span>{order.billing.address_1}</span>
-                    <span>{order.billing.country}</span>
-                    <span>{order.billing.city}</span>
-                    <span>{order.billing.phone}</span>
-                  </div>
+            </div>
+            <div className="border border-gray">
+              <h5 className="text-h5 pt-4 md:pt-6 px-4 sm:px-6 pb-4">
+                Адрес доставки
+              </h5>
+              <Separator className="w-full" />
+              <div className="flex flex-col gap-4 p-4 sm:p-6">
+                <span className="text-button-small sm:text-body1 capitalize">
+                  {order.billing.first_name} {order.billing.last_name}
+                </span>
+                <div className="flex flex-col gap-2 text-button-small sm:text-body2 capitalize text-gray-dark text-body2">
+                  <span>{order.billing.address_1}</span>
+                  <span>{order.billing.country}</span>
+                  <span>{order.billing.city}</span>
+                  <span>{order.billing.phone}</span>
                 </div>
               </div>
+            </div>
+            <div className="w-full flex flex-col-reverse md:flex-row lg:hidden md:col-span-3 gap-4">
+              <Button
+                className="w-full"
+                onClick={handleRepeatOrder}
+                size="medium"
+                disabled={isPending}
+              >
+                {isPending && <LoaderIcon className="animate-spin" />}
+                Повторить заказ
+              </Button>
+              <RefundButton
+                order={order}
+                disabled={!!getReturnDisabled()}
+                onSuccess={() => {
+                  setSuccessRefund(true);
+                }}
+              >
+                <div
+                  className="w-full"
+                  data-tooltip-id="refund"
+                  data-tooltip-content={getReturnDisabled()}
+                >
+                  <Button
+                    className="w-full"
+                    variant="secondary"
+                    size="medium"
+                    disabled={!!getReturnDisabled()}
+                  >
+                    Оформить возврат
+                  </Button>
+                  <StyledTooltip place="top" id="refund" />
+                </div>
+              </RefundButton>
+              <SuccessRefundDialog
+                open={successRefund}
+                setOpen={setSuccessRefund}
+              />
             </div>
           </div>
         </AccordionContent>
--- src/features/Orders/components/OrderProductCard.tsx
+++ src/features/Orders/components/OrderProductCard.tsx
@@ -8,46 +8,44 @@ const OrderProductCard = ({ product }: { product: OrderProductItem }) => {
   const size = product.meta_data.find((item) => item.key === "pa_size");
 
   return (
-    <article className="flex gap-12 justify-between items-center py-8 pr-4 border-b border-b-gray last:border-b-0">
-      <div className="flex gap-6 items-center">
+    <article className="flex flex-col md:flex-row gap-8 lg:gap-12 justify-between md:items-center py-4 sm:py-8 md:pr-4 border-b border-b-gray last:border-b-0">
+      <div className="flex gap-4 lg:gap-6 sm:items-center">
         <ImageWithLoader
           src={product.image.src}
           textClassName="text-sm"
           wrapperClassName="w-[96px] h-[128px]"
           alt=""
         />
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col justify-between sm:justify-normal items-start gap-4">
           <OrderProductDialog orderProduct={product}>
             <h5 className="text-h5 cursor-pointer">{product.name}</h5>
           </OrderProductDialog>
-          <div className="flex gap-4 text-gray-middle">
+          <div className="hidden sm:flex gap-4 text-gray-middle">
             <span>Артикул:</span>
             <span>{product.sku}</span>
           </div>
-          <div className="flex gap-6">
-            <div className="flex items-center gap-4">
+          <div className="flex flex-col sm:flex-row gap-2 sm:gap-6">
+            <div className="flex text-description sm:text-body2 items-center gap-4">
               <span>Размер</span>
               <Separator className="h-4/6" orientation="vertical" />
-              <span className="text-gray-middle text-body2 whitespace-nowrap">
+              <span className="text-gray-middle whitespace-nowrap">
                 {size?.display_value}
               </span>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center text-description sm:text-body2 gap-4">
               <span>Цвет</span>
               <Separator className="h-4/6" orientation="vertical" />
-              <span className="text-gray-middle text-body2">
-                {color?.display_value}
-              </span>
+              <span className="text-gray-middle">{color?.display_value}</span>
             </div>
           </div>
         </div>
       </div>
-      <div className="max-w-[600px] w-full grid grid-cols-3 items-center">
+      <div className="md:max-w-[333px] lg:max-w-[600px] w-full grid grid-cols-3 items-center">
         <span>{product.price} ₸</span>
-        <div className="bg-gray-light text-body2 p-2 justify-self-center">
+        <div className=" bg-gray-light text-button-xs sm:text-body2 p-2 justify-self-center">
           {product.quantity} шт
         </div>
-        <span className="justify-self-end">{product.total} ₸</span>
+        <span className="justify-self-end text-nowrap">{product.total} ₸</span>
       </div>
     </article>
   );
